444287ab71ee6dbc288211babcff32ca52d7ce15
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-08-04 14:03:33 -0500

    added to the homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,9 +25,27 @@ router.get('/', async (req, res) => {
 });
 router.get('/profile', withAuth, async(req, res) => {
   try {
-    console.log();
+    const userData = await findByPK(req.session.user_id,{
+      attributes: {exclude: ['password'] },
+      include: [{model: Goal}],
+    });
+    const user = userData.get({plain: true});
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
   } catch (err) {
     res.status(500).json(err);
   }
+});
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
 });
 
