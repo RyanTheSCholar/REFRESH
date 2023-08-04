44b83f734a0eaa38d36bcf2e829ed3bb3469361e
Lilibeth Mejia <mejialily036@gmail.com>
2023-08-04 14:03:44 -0500

    made changes

--- controllers/api/goalRoutes.js
+++ controllers/api/goalRoutes.js
@@ -2,59 +2,59 @@ const router = require('express').Router();
 const { Goal } = require('../../models');
 
 router.post('/', withAuth, async (req, res) => {
-    try {
-      const goalData = await Goal.create({
-        ...req.body,
-        user_id: req.session.user_id,
-        
-      });
+  try {
+    const goalData = await Goal.create({
+      ...req.body,
+      user_id: req.session.user_id,
 
-      res.status(200).json(goalData);
-    } catch (err) {
-      console.log(err);
-      res.status(400).json(err);
-    }
-  });
+    });
 
-  router.put('/:id', async (req, res) => {
-    try {
-      console.log("PUT route");
-      const blogData = await BlogPost.update(
-        req.body,
-        {
-          where: {
-            id: req.params.id,
-          },
-        }
-  
-      )
-      return res.json(blogData)
-  
-    } catch (err) {
-      console.log(err);
-      res.status(400).json(err);
-    }
-  });
+    res.status(200).json(goalData);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    // console.log('PUT route');
+    const goalData = await Goal.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+
+    );
+    res.status(200).json(goalData);
+
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 
 router.delete('/:id', withAuth, async (req, res) => {
-    // delete a goal by its `id` value
-    try {
-        const goalData = await Goal.destroy({
-            where: {
-                id: req.params.id,
-                // user_id: req.session.user_id,
-            },
-        });
-        if (!goalData) {
-            res.status(404).json({ message: 'No goal found with this id!' });
-            return;
-        }
-        res.status(200).json(goalData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+  // delete a goal by its `id` value
+  try {
+    const goalData = await Goal.destroy({
+      where: {
+        id: req.params.id,
+        // user_id: req.session.user_id,
+      },
+    });
+    if (!goalData) {
+      res.status(404).json({ message: 'No goal found with this id!' });
+      return;
     }
+    res.status(200).json(goalData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 
