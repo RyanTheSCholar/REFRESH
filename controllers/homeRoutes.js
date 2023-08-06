eba0e3d35cec846081cfad35cfed59269d006d16
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-08-06 13:49:45 -0500

    added homeRoutes and index.js in the controllers folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,29 +3,42 @@ const router = require('express').router();
 const { Category, Goal, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/:category', async (req, res) => {
   try {
-    const goalsData = await Goal.findAll({
-      include: [
-        {
-          model: User,
-          attribute: ['name']
-        },
-      ],
-    });
-    const goals = goalsData.map((goals) => goals.get({plain: true}));
-
-    res.render('homepage', {
-      goals,
-      logged_in: req.session.logged_in
-    });
+    if(req.params.category){
+      const goalsData = await Category.findOne(req.params.category, {
+        include: [
+          {
+            model: Goal
+          },
+        ],
+      });
+      const goals = goalsData.map((goals) => goals.get({plain: true}));
+      res.render('homepage', {
+        goals,
+        logged_in: req.session.logged_in
+      });
+    }else{
+      const goalsData = await Goal.findAll({
+        include: [
+          {
+            model: Goal,
+          }
+        ]
+      });
+      const goals = goalsData.map((goals) => goals.get({plain: true}));
+      res.render('homepage', {
+        goals,
+        logged_in: req.session.logged_in
+      });
+    }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 router.get('/profile', withAuth, async(req, res) => {
   try {
-    const userData = await findByPK(req.session.user_id,{
+    const userData = await User.findByPk(req.session.user_id,{
       attributes: {exclude: ['password'] },
       include: [{model: Goal}],
     });
@@ -45,7 +58,5 @@ router.get('/login', (req, res) => {
     res.redirect('/profile');
     return;
   }
-
   res.render('login');
 });
-
