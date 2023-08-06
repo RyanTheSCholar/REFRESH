f0e85f1b5018a634331f28f3a6adb53f173718c8
Lilibeth Mejia <mejialily036@gmail.com>
2023-08-06 18:13:05 -0500

    added api- routes code

--- controllers/api/goalRoutes.js
+++ controllers/api/goalRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Goal } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -11,9 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({helpers});
-
-
-
 
 const sess = {
   secret: 'Super secret secret',
