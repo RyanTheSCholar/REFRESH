f1055b1a2e2be5e10686563458b3ac692a09b49c
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-08-08 21:11:38 -0500

    added better css, seeds and server.js

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({helpers});
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 300000,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
